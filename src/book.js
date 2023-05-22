64e8575a20df8d35babdb975f30bebc89575a7dd
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-05-22 10:46:29 +0530

    some changes

--- src/book.js
+++ src/book.js
@@ -2,16 +2,23 @@ const Book = (props) => {
   const { img, title, author, addToCard, buyNow, getBook, id, index } = props;
 
   //console.log(props)
-  return <article className='book'>
-    <span className="index">#{index + 1}</span>
-    <img src={img} alt={title} />
-    <h2>{title}</h2>
-    <h3>{author}</h3>
-    <button className="btn" onClick={buyNow}>Buy Now</button>
-    <button className="btn bttn" onClick={addToCard}>Add To Card</button>
-    <button className="btn bttn" onClick={() => getBook(id)}>Get Book</button>
+  return (
+    <article className="book">
+      <span className="index">#{index + 1}</span>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <button className="btn" onClick={buyNow}>
+        Buy Now
+      </button>
+      <button className="btn bttn" onClick={addToCard}>
+        Add To Card
+      </button>
+      <button className="btn bttn" onClick={() => getBook(id)}>
+        Get Book
+      </button>
+    </article>
+  );
+};
 
-  </article>
-}
-
-export default Book
+export default Book;
